f989eee82e167a8e8a97169a7885aa1639f34d69
Baljeet <baljeet@ameotech.com>
2018-10-18 16:32:53 +0530

    fixed for django

--- djangotoolbox/fields.py
+++ djangotoolbox/fields.py
@@ -1,7 +1,12 @@
 # All fields except for BlobField written by Jonas Haag <jonas@lophus.org>
 
 from django.core.exceptions import ValidationError
-from django.utils.importlib import import_module
+try:
+    from importlib import import_module
+except ImportError:
+    # Fallback for Python 2.5 and 2.6
+    from django.utils.importlib import import_module
+    
 from django.db import models
 from django.db.models.fields.subclassing import Creator
 from django.db.utils import IntegrityError
